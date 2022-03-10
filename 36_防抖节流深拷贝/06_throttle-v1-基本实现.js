59493388362698cc09393e6647e7c306fe05a3fa
zhangzhibang0309 <295854713@qq.com>
2022-03-11 01:42:02 +0800

    throttle

--- 36_防抖节流深拷贝/06_throttle-v1-基本实现.js
+++ 36_防抖节流深拷贝/06_throttle-v1-基本实现.js
@@ -1,12 +1,21 @@
 function throttle(fn, interval) {
+  // 记录上一次触发事件的时间，只有第一次赋值的时候会执行，后面lastTimer只会在remainTime <= 0之后被赋值
+  const { leading, trailing } = options;
   let lastTime = 0;
 
+  // 这个函数会被返回，绑定到某个事件上面，比如oninput
   const _throttle = function () {
+    // 拿到当前时间
     const nowTime = new Date().getTime();
-    const remainTime = interval - (nowTime - lastTime);
 
+    // 剩余多长时间执行 = 设置好的间隔 - （现在的时间 - 上次触发事件的事件）
+    const remainTime = interval - (nowTime - lastTime); // 第一次执行或者是间隔了一段时间再次执行的话，一去触发事件会立马执行，将数据带入这个公式仔细算一下就知道，这两种情况下，remainTime是负数（因为nowTime这个时间戳巨大）
+
+    // 如果remainTime <= 0了，说明现在已经距离上次事件触发经过一个interval了，而且直到目前位置，还有事件在不断尝试触发，所i一应该进行下一次事件触发了
     if (remainTime <= 0) {
+      // 真正需要执行的函数
       fn();
+      // 上面说了，要更新一下lastTime，让它等于此刻的时间
       lastTime = nowTime;
     }
   };
---
+++ 36_防抖节流深拷贝/07_throttle-v2-leading实现.js
@@ -0,0 +1,24 @@
+function throttle(fn, interval, options = { leading: true, trailing: false }) {
+  // 默认leading是true
+
+  const { leading, trailing } = options;
+  let lastTime = 0;
+
+  // 这个函数会被返回，绑定到某个事件上面，比如oninput
+  const _throttle = function () {
+    const nowTime = new Date().getTime();
+
+    // 检测是否是第一次，以及leading是否是false
+    // 满足条件，第一次不加载，让lastTime = nowTime，算出来第一次interval是0
+    if (lastTime === 0 && leading === false) lastTime = nowTime;
+
+    if (remainTime <= 0) {
+      // 真正需要执行的函数
+      fn();
+      // 上面说了，要更新一下lastTime，让它等于此刻的时间
+      lastTime = nowTime;
+    }
+  };
+
+  return _throttle;
+}
