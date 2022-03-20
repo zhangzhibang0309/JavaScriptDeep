f3eb562dd15e6b408244a7d32b6c7b8de7031dbb
zhangzhibang0309 <295854713@qq.com>
2022-03-21 01:53:40 +0800

    ts接口的使用

---
+++ 38_TypeScript/09_TypeScript接口的使用/01_声明对象类型.ts
@@ -0,0 +1,25 @@
+// 通过类型别名来声明对象类型
+// type InfoType = { name: string, age: number }
+
+// 另外一种方式声明对象类型: 接口interface
+// 在其中可以定义可选类型
+// 也可以定义只读属性
+interface IInfoType {
+    readonly name: string
+    age: number
+    friend?: {
+        name: string
+    }
+}
+
+const info: IInfoType = {
+    name: "zzb",
+    age: 18,
+    friend: {
+        name: "kobe"
+    }
+}
+
+console.log(info.friend?.name)
+console.log(info.name)
+info.age = 20
---
+++ 38_TypeScript/09_TypeScript接口的使用/05_交叉类型.ts
@@ -0,0 +1,35 @@
+// 一种组合类型的方式：联合类型
+type ZzbType = number | string
+type Direction = "left" | "right" | "center"
+
+
+// 另一种组件类型的方式：交叉类型
+type WType = number & string
+
+interface ISwim {
+    swimming: () => void
+}
+
+interface IFly {
+    flying: () => void
+}
+
+type MyType1 = ISwim | IFly
+type MyType2 = ISwim & IFly
+
+const obj1: MyType1 = {
+    flying() {
+
+    }
+}
+
+// 类似于多接口继承
+const obj2: MyType2 = {
+    swimming() {
+
+    },
+    flying() {
+
+    }
+}
+export { }
